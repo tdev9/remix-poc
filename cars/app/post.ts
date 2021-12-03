e296b0d09a4e0be966b769a893e2b1ea20697318
tdev <tdevvv9@gmail.com>
2021-12-03 18:12:04 +0100

    Add posts md

--- cars/app/post.ts
+++ cars/app/post.ts
@@ -1,18 +1,46 @@
+import path from "path";
+import fs from "fs/promises";
+import parseFrontMatter from "front-matter";
+import invariant from "tiny-invariant";
+
 export type Post = {
-    slug: string;
-    title: string;
-  };
-  
-  export function getPosts() {
-    let posts: Post[] = [
-      {
-        slug: "my-first-post",
-        title: "My First Post"
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I Made Just For You"
-      }
-    ];
-    return posts;
-  }
+  slug: string;
+  title: string;
+};
+
+export type PostMarkdownAttributes = {
+  title: string;
+};
+
+
+function isValidPostAttributes(
+  attributes: any
+): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
+
+const postsPath = path.join(__dirname, "..", "posts");
+
+export async function getPosts() {
+  const dir = await fs.readdir(postsPath);
+  return Promise.all(
+    dir.map(async filename => {
+      const file = await fs.readFile(
+        path.join(postsPath, filename)
+      );
+      const { attributes } = parseFrontMatter(
+        file.toString()
+      );
+
+      invariant(
+        isValidPostAttributes(attributes),
+        `${filename} has bad meta data!`
+      );
+      
+      return {
+        slug: filename.replace(/\.md$/, ""),
+        title: attributes.title
+      };
+    })
+  );
+}
